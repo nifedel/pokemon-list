b30cc354c1e247b5575d87a4cf5c1c878305dee4
Ladar <mishamironov5@mail.ru>
2025-01-20 14:51:52 +0300

    My pull request

--- app/src/main/java/com/example/poketlist/MainActivity.kt
+++ app/src/main/java/com/example/poketlist/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PoketListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android1",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
